d09e8dfd56505140b643b186fab0549d6c70f302
sebprt <sebastien.parrat@orange.fr>
2023-11-28 18:30:00 +0100

    Changed throw exception

--- pwa/components/PieceDatagrid.tsx
+++ pwa/components/PieceDatagrid.tsx
@@ -1,5 +1,8 @@
-import {Datagrid, useRecordContext} from "@api-platform/admin";
-import { fetchUtils } from 'ra-core';
+import { useRecordContext } from 'react-admin';
+import {fetchUtils, useGetList, useGetOne, WithListContext} from 'ra-core';
+import {Chip, List, TextField} from "@mui/material";
+import {Datagrid} from "ra-ui-materialui";
+import {Stack} from "@mui/system";
 
 const apiUrl = 'https://localhost'; // Remplacez par l'URL de votre API
 const httpClient = fetchUtils.fetchJson;
@@ -8,14 +11,25 @@ const PieceDatagrid = props => {
   const record = useRecordContext();
   if (!record) return null;
 
-  console.log(record)
-  const url = `${apiUrl}/puzzles/${record.id}/pieces`;
-  const data = httpClient(url).then(({json}) => ({
-    data: json,
-  }));
+  const url = `${apiUrl}/puzzles/${record.originId}/pieces`;
+  const httpClient = (url: string, options = {}) => {
+    if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/ld+json' });
+    }
+    return fetchUtils.fetchJson(url, options);
+  };
 
+  const data = httpClient(url).then(({ json }) => ({
+    pieces: json.pieces,
+  }));
   return (
-   <div></div>
+      <List
+          data={data}
+      >
+        <Datagrid>
+          <TextField source="id" />
+        </Datagrid>
+      </List>
   )
 };
 
